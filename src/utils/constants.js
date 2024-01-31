301f834d35f2f46b1bffc24f0b7bdffda5150872
Georgi957 <gesh007@abv.bg>
2024-01-31 11:54:55 +0200

    Setup jwt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 const ENV = {
     PORT: 3000,
-    DB_URI: "mongodb://127.0.0.1:27017/courseBook"
+    DB_URI: "mongodb://127.0.0.1:27017/courseBook",
+    SECRET: "567e8d6b-4d8b-4ec3-b19f-6486dd89708b"
 }
 
 module.exports = ENV;
