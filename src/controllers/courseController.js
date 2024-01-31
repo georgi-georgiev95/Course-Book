8a101a96a1260a5cf09eb5db7249615487bed374
Georgi957 <gesh007@abv.bg>
2024-01-31 13:16:23 +0200

    Create course controller

---
+++ src/controllers/courseController.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const courseService = require('../services/courseService');
+
+router.get('/dashboard', async (req, res) => {
+    
+});
+
+module.exports = router;
