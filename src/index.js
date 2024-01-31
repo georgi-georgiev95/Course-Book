c583f03c1de7bb99fcfd0720d757db3a4a00bec0
Georgi957 <gesh007@abv.bg>
2024-01-31 11:02:18 +0200

    Add router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
+const router = require("./router");
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running on port 3000..."));
