d1a3503b7a088cfabbe52657e50a666602d1849b
Georgi957 <gesh007@abv.bg>
2024-01-31 13:53:10 +0200

    Create course and save it in DB

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -11,6 +11,19 @@ router.get('/dashboard', async (req, res) => {
 
 router.get('/create', (req, res) => {
     res.render('courses/create');
+});
+
+router.post('/create', async (req, res) => {
+    const courseData = {
+        ...req.body,
+        owner: req.user.id 
+    };
+    try {
+        await courseService.create(courseData);
+        res.redirect('/courses/dashboard');
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 module.exports = router;
--- src/services/courseService.js
+++ src/services/courseService.js
@@ -1,3 +1,5 @@
 const Course = require('../models/Course');
 
 exports.getAll = () => Course.find();
+
+exports.create = (courseData) => Course.create(courseData);
