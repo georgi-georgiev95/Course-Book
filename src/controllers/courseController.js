74f771194f3281b264a3ded7c79fa596393b19ff
Georgi957 <gesh007@abv.bg>
2024-01-31 13:31:05 +0200

    Adjust courseController

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -3,7 +3,14 @@ const router = require("express").Router();
 const courseService = require('../services/courseService');
 
 router.get('/dashboard', async (req, res) => {
-    
+    const courses = await courseService.getAll().lean();
+
+    res.render('courses/catalog', { courses });
+
 });
+
+router.get('/create', (req, res) => {
+    res.render('courses/create');
+})
 
 module.exports = router;
